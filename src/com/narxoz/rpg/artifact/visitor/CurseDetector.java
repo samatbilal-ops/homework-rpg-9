758c7034cb4e2d526da85d31b39a6b38703b9de7
Bilal Samat <samat.bilal@narxoz.kz>
2026-05-03 17:55:45 +0500

    Add Armor

--- src/com/narxoz/rpg/artifact/visitor/CurseDetector.java
+++ src/com/narxoz/rpg/artifact/visitor/CurseDetector.java
@@ -1,4 +1,64 @@
 package com.narxoz.rpg.artifact.visitor;
 
-public class CurseDetector {
+import com.narxoz.rpg.artifact.*;
+
+public class CurseDetector implements ArtifactVisitor {
+
+    private int suspiciousCount = 0;
+
+    @Override
+    public void visit(Weapon weapon) {
+        if (weapon.getAttackBonus() < 0) {
+            suspiciousCount++;
+            System.out.printf("  [Curse] Weapon '%s' >> CORRUPTED | negative attack bonus detected%n", weapon.getName());
+        } else {
+            System.out.printf("  [Curse] Weapon '%s' >> clear%n", weapon.getName());
+        }
+    }
+
+    @Override
+    public void visit(Potion potion) {
+        if (potion.getHealing() < 0) {
+            suspiciousCount++;
+            System.out.printf("  [Curse] Potion '%s' >> TAINTED | inflicts harm on consumption%n", potion.getName());
+        } else {
+            System.out.printf("  [Curse] Potion '%s' >> clear%n", potion.getName());
+        }
+    }
+
+    @Override
+    public void visit(Scroll scroll) {
+        boolean dark = scroll.getSpellName().toLowerCase().contains("curse")
+                || scroll.getSpellName().toLowerCase().contains("death")
+                || scroll.getSpellName().toLowerCase().contains("drain");
+        if (dark) {
+            suspiciousCount++;
+            System.out.printf("  [Curse] Scroll '%s' >> FORBIDDEN | dark inscription [%s] identified%n",
+                    scroll.getName(), scroll.getSpellName());
+        } else {
+            System.out.printf("  [Curse] Scroll '%s' >> inscription appears harmless%n", scroll.getName());
+        }
+    }
+
+    @Override
+    public void visit(Ring ring) {
+        if (ring.getMagicBonus() < 0) {
+            suspiciousCount++;
+            System.out.printf("  [Curse] Ring '%s' >> HEXED | siphons magical reserves%n", ring.getName());
+        } else {
+            System.out.printf("  [Curse] Ring '%s' >> clear%n", ring.getName());
+        }
+    }
+
+    @Override
+    public void visit(Armor armor) {
+        if (armor.getDefenseBonus() < 0) {
+            suspiciousCount++;
+            System.out.printf("  [Curse] Armor '%s' >> WEAKENED | protection is compromised%n", armor.getName());
+        } else {
+            System.out.printf("  [Curse] Armor '%s' >> clear%n", armor.getName());
+        }
+    }
+
+    public int getSuspiciousCount() { return suspiciousCount; }
 }
