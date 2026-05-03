fe1333fa516ee1ee7a6465c8f19678d8a9da9a09
Bilal Samat <samat.bilal@narxoz.kz>
2026-05-03 18:01:40 +0500

    Add WeightCalculator

--- src/com/narxoz/rpg/artifact/visitor/WeightCalculator.java
+++ src/com/narxoz/rpg/artifact/visitor/WeightCalculator.java
@@ -1,4 +1,40 @@
 package com.narxoz.rpg.artifact.visitor;
 
-public class WeightCalculator {
+import com.narxoz.rpg.artifact.*;
+
+public class WeightCalculator implements ArtifactVisitor {
+
+    private int totalWeight = 0;
+
+    @Override
+    public void visit(Weapon weapon) {
+        totalWeight += weapon.getWeight();
+        System.out.printf("  [Weight] Weapon '%s' carries %d kg%n", weapon.getName(), weapon.getWeight());
+    }
+
+    @Override
+    public void visit(Potion potion) {
+        totalWeight += potion.getWeight();
+        System.out.printf("  [Weight] Potion '%s' carries %d kg%n", potion.getName(), potion.getWeight());
+    }
+
+    @Override
+    public void visit(Scroll scroll) {
+        totalWeight += scroll.getWeight();
+        System.out.printf("  [Weight] Scroll '%s' carries %d kg%n", scroll.getName(), scroll.getWeight());
+    }
+
+    @Override
+    public void visit(Ring ring) {
+        totalWeight += ring.getWeight();
+        System.out.printf("  [Weight] Ring '%s' carries %d kg%n", ring.getName(), ring.getWeight());
+    }
+
+    @Override
+    public void visit(Armor armor) {
+        totalWeight += armor.getWeight();
+        System.out.printf("  [Weight] Armor '%s' carries %d kg%n", armor.getName(), armor.getWeight());
+    }
+
+    public int getTotalWeight() { return totalWeight; }
 }
